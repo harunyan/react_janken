b295911af836ec8cb10a3d67f3f58cad6680c06d
Morimoto Haruhi <morimoto.haruhi@gmail.com>
2025-07-26 16:29:20 +0900

    ReactRouterを削除

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider, createBrowserRouter} from "react-router-dom";
 import './index.css'
 import App from './App.tsx'
 
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <App/>,
-        children: [
-            {
-                path: '',
-                element: <div>ホーム画面</div>,
-            }
-        ]
-    },
-])
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <App />
   </React.StrictMode>,
 )
